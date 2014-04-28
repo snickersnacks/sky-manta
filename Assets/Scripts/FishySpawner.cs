dae8ba669ae63f93b2f285fe2aceec3ede43801e
zite <zite00@gmail.com>
2014-04-28 14:31:58 -0700

    fish in. scaling correctly. world moved.

--- Assets/Scripts/FishySpawner.cs
+++ Assets/Scripts/FishySpawner.cs
@@ -11,9 +11,10 @@ public class FishySpawner : MonoBehaviour
 		for (int ctr = 0; ctr < 100; ctr++)
 		{
 			Transform fishy = (Transform)GameObject.Instantiate(Fishy);
-			fishy.position = new Vector3(Random.Range(-500, 500), Random.Range(-50, 0), Random.Range(-500, 500));
+			fishy.position = new Vector3(Random.Range(-1000, 1000), Random.Range(-200, -25), Random.Range(-1000, 1000));
 			fishy.eulerAngles = new Vector3(Random.Range(-360, 360), Random.Range(-360, 360), Random.Range(-360, 360));
-			fishy.transform.localScale = new Vector3(Random.Range(1, 5), Random.Range(1, 5), Random.Range(1, 5));
+
+			fishy.transform.localScale = Vector3.one * Random.Range(1, 5);
 		}
 	}
 }
